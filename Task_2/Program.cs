fd495bc4468ff274a5d68cbabf60defe4b009791
terminator <mikkhailesku@mail.com>
2023-09-15 20:35:48 +0300

    Try again

---
+++ Task_2/Program.cs
@@ -0,0 +1,28 @@
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+Console.WriteLine("Enter number: ");
+string strNum = Console.ReadLine();
+int num1 = int.Parse(strNum);
+
+Console.WriteLine("Enter number: ");
+strNum = Console.ReadLine();
+int num2 = int.Parse(strNum);
+
+Console.WriteLine("Enter number: ");
+strNum = Console.ReadLine();
+int num3 = int.Parse(strNum);
+
+int max = num1;
+
+if (num1 > num2) max = num1;
+
+if (max < num3) max = num3;
+
+if (num1 < num2) max = num2;
+
+if (max < num3) max = num3;
+
+Console.WriteLine(max);
---
+++ Task_3/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+//4 -> да
+//-3 -> нет
+//7 -> нет
+
+Console.WriteLine("Enter number: ");
+string strNum = Console.ReadLine();
+int num = int.Parse(strNum);
+
+int result = num / 2;
+
+if (result == num)
+{
+    System.Console.WriteLine("да");
+}
+else
+{
+    System.Console.WriteLine("нет");
+}
